23beb40923b32ddb45d01d76acf498e35c218094
daytolp <yashirorko@gmail.com>
2025-11-06 16:21:36 -0600

    modificacion en test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,6 +11,7 @@ public class BasicApplicationTests  {
     @Test
     void contextLoads() {
        String message = "Default Message Cambio 11";
-        assertNotNull(message);
+       System.out.println("Mensaje: " + message);
+       assertNotNull(message);
     }
 }
